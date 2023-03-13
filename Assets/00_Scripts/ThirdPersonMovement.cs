65d2664b4ec02cbc5668076ed3cf5c653648e614
AhmedMuraat <95277929+AhmedMuraat@users.noreply.github.com>
2023-03-13 18:29:17 +0100

    Animations
    Animation of sprinting walking and idle is complete,
    
    Jumping animation needs to be implemented
    
    sneaking script and animation should be implemented

--- Assets/00_Scripts/ThirdPersonMovement.cs
+++ Assets/00_Scripts/ThirdPersonMovement.cs
@@ -7,12 +7,15 @@ public class ThirdPersonMovement : MonoBehaviour
 {
     // Start is called before the first frame update
     public Transform cam;
+
+    Animator anim;
     CharacterController characterController;
     float turnSmoothTime = .1f;
     float turnSmoothVelocity;
     Vector2 movement;
-    public float walkSpeed = 4f;
-    public float sprintSpeed = 8f;
+    public float walkSpeed = 3f;
+    public float sprintSpeed = 5f;
+    bool sprinting;
     private float trueSpeed;
 
     public float jumpHeight = 2f;
@@ -22,6 +25,7 @@ public class ThirdPersonMovement : MonoBehaviour
     void Start()
     {
         trueSpeed = walkSpeed;
+        anim = GetComponentInChildren<Animator>();
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;    
         characterController = GetComponent<CharacterController>();
@@ -31,7 +35,7 @@ public class ThirdPersonMovement : MonoBehaviour
     void Update()
     {
         isGrounded = Physics.CheckSphere(transform.position, .1f, 1);
-
+        anim.SetBool("IsGrounded", isGrounded);
         if (isGrounded && velocity.y < 0)
         {
             velocity.y = -1;
@@ -44,13 +48,17 @@ public class ThirdPersonMovement : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.LeftShift))
         {
             trueSpeed = sprintSpeed;
+            sprinting = true;
         }
         if (Input.GetKeyUp(KeyCode.LeftShift))
         {
             trueSpeed = walkSpeed;
+            sprinting = false;
 
-        }  
-        
+        }
+
+        anim.transform.localPosition = Vector3.zero;
+        anim.transform.localEulerAngles = Vector3.zero;
         if (direction.magnitude >= 0.1f)
         {
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
@@ -59,6 +67,21 @@ public class ThirdPersonMovement : MonoBehaviour
            
             Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
             characterController.Move(moveDirection.normalized * trueSpeed * Time.deltaTime);
+            if (sprinting == true)
+            {
+                anim.SetFloat("Speed", 2);
+
+            }
+            else
+            {
+                anim.SetFloat("Speed", 1);
+            }
+          
+        }
+        else
+        {
+            anim.SetFloat("Speed", 0);
+
         }
         if (Input.GetButtonDown("Jump") && isGrounded)
         {
