0b4646b9ec114f991747b5efcca11217ab90ba71
AhmedMuraat <95277929+AhmedMuraat@users.noreply.github.com>
2023-03-13 17:18:57 +0100

    sprinting + jumping

--- Assets/00_Scripts/ThirdPersonMovement.cs
+++ Assets/00_Scripts/ThirdPersonMovement.cs
@@ -1,28 +1,74 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class ThirdPersonMovement : MonoBehaviour
 {
     // Start is called before the first frame update
-
+    public Transform cam;
     CharacterController characterController;
+    float turnSmoothTime = .1f;
+    float turnSmoothVelocity;
     Vector2 movement;
-    public float moveSpeed;
+    public float walkSpeed = 4f;
+    public float sprintSpeed = 8f;
+    private float trueSpeed;
+
+    public float jumpHeight = 2f;
+    public float gravity =-4f;
+    bool isGrounded;
+    Vector3 velocity;
     void Start()
     {
+        trueSpeed = walkSpeed;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;    
         characterController = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        isGrounded = Physics.CheckSphere(transform.position, .1f, 1);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -1;
+        }
+
+
         movement = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
         Vector3 direction = new Vector3(movement.x, 0, movement.y).normalized;
 
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            trueSpeed = sprintSpeed;
+        }
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            trueSpeed = walkSpeed;
+
+        }  
+        
         if (direction.magnitude >= 0.1f)
         {
-            characterController.Move(direction * moveSpeed * Time.deltaTime);
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+           
+            Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            characterController.Move(moveDirection.normalized * trueSpeed * Time.deltaTime);
         }
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt((jumpHeight * 10) * -2 * gravity);
+        }
+        if (velocity.y > -20)
+        {
+            velocity.y += (gravity * 10) * Time.deltaTime;
+        }
+    
+        characterController.Move(velocity * Time.deltaTime);
     }
 }
