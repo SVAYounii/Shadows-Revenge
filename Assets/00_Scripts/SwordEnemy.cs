548eae01a1ebb556fdb544886f1ee9b5239710fa
SVAYounii <younes048@gmail.com>
2023-03-15 23:45:43 +0100

    Knife enemy done
    the entire enemy is done

--- Assets/00_Scripts/SwordEnemy.cs
+++ Assets/00_Scripts/SwordEnemy.cs
@@ -1,8 +1,11 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.SocialPlatforms;
+using static UnityEditor.PlayerSettings;
 
 public class SwordEnemy : HitAble
 {
@@ -13,18 +16,34 @@ public class SwordEnemy : HitAble
 
     [Header("Attack Settings")]
     public float AttackDistance;
+    public bool HasWeapon;
+
+    [Header("Vision Settings")]
+    public float radius;
+    [Range(0, 180)]
+    public float angle;
+    public LayerMask targetMask;
+    public LayerMask obstructionMask;
+    public bool canSeePlayer;
+
     Animator animator;
+    bool _hasAttackPerformed;
+    bool _isReadyForAttack = true;
+    float _nextTimeAttack;
 
     [Header("UI Settings")]
     public GameObject UI;
 
-    GameObject _player;
+    [HideInInspector]
+    public GameObject Player;
 
     float _nextTime;
 
     int _index = 0;
     bool _isWalking = false;
-
+    bool _playerIsClose;
+    bool _once;
+    Vector3 randomDirection;
     public enum State
     {
         Roaming,
@@ -38,12 +57,13 @@ public class SwordEnemy : HitAble
     void Start()
     {
         EnemyState = State.Roaming;
-        _player = GameObject.FindGameObjectWithTag("Player");
+        Player = GameObject.FindGameObjectWithTag("Player");
         animator = this.gameObject.GetComponent<Animator>();
         if (WalkPlaces.Count > 0 && Agent != null)
         {
             StartWalking();
         }
+        StartCoroutine(FOVRoutine());
     }
 
     // Update is called once per frame
@@ -52,54 +72,187 @@ public class SwordEnemy : HitAble
         switch (EnemyState)
         {
             case State.Roaming:
+                if (canSeePlayer && HasWeapon)
+                {
+                    EnemyState = State.Chasing;
+                    break;
+                }
+                else if (canSeePlayer && !HasWeapon)
+                {
+                    EnemyState = State.Fleeing;
+                }
+
                 Roaming();
-                break; 
+                break;
             case State.Chasing:
                 Chase();
-                break; 
+                break;
             case State.Attacking:
                 Attacking();
                 break;
             case State.Fleeing:
+
                 Fleeing();
                 break;
         }
-        
-        UI.transform.rotation = Quaternion.LookRotation(transform.position - Camera.main.transform.position);
 
+        if (_isWalking && EnemyState == State.Roaming)
+        {
+            Agent.isStopped = false;
+            animator.SetInteger("State", 1);
+
+        }
+        else if (!_isWalking && EnemyState == State.Roaming)
+        {
+            Agent.isStopped = true;
+            animator.SetInteger("State", 0);
+
+        }
+        UI.transform.rotation = Quaternion.LookRotation(transform.position - Camera.main.transform.position);
+     
+    }
+
+    void ResetNavMesh()
+    {
+        if (Agent.hasPath)
+        {
+            Agent.ResetPath();
+        }
     }
 
     void Fleeing()
     {
+        Debug.Log("Running Away");
+        Agent.speed = 3.5f;
+
+        if (!_once)
+        {
+            _once = true;
+            ResetNavMesh();
+        }
+
+        float distance = Vector3.Distance(transform.position, Player.transform.position);
+        if (distance < 20)
+        {
+            _playerIsClose = true;
+        }
+        else
+        {
+            _playerIsClose = false;
+        }
+
+        if (_playerIsClose && AgentHasArrived())
+        {
+            randomDirection = UnityEngine.Random.insideUnitSphere * 50;
+            randomDirection += Player.transform.position;
+            NavMeshHit hit;
+            if (NavMesh.SamplePosition(randomDirection, out hit, 20, 1))
+            {
+                RunTo(hit.position);
+            }
+            animator.SetInteger("State", 2);
+        }
+        else if (!_playerIsClose && AgentHasArrived())
+        {
+            animator.SetInteger("State", 3);
+        }
+
+
+    }
+
+    void RunTo(Vector3 pos)
+    {
+        Agent.isStopped = false;
+        animator.SetInteger("State", 2);
+        Agent.SetDestination(pos);
+    }
+    void WalkTo(Vector3 pos)
+    {
+        _isWalking = true;
+        Agent.isStopped = false;
         animator.SetInteger("State", 1);
+        Agent.SetDestination(pos);
+    }
+
+    bool AgentHasArrived()
+    {
+        if (!Agent.hasPath)
+            return true;
+
+        float dist = Agent.remainingDistance;
+
+        if (dist != Mathf.Infinity && Agent.pathStatus == NavMeshPathStatus.PathComplete && dist <= Agent.stoppingDistance +0.5f)
+        {
+            return true;
+
+        }
+        return false;
     }
 
     void Attacking()
     {
-        animator.SetInteger("State", 2);
+
+        if (!_hasAttackPerformed)
+        {
+            //is currently in the attack animation
+            animator.SetTrigger("Attack");
+            _hasAttackPerformed = true;
+            _nextTimeAttack = Time.time + 2.5f;
+            Debug.Log("Attacking player");
+        }
+        else if (Time.time > _nextTimeAttack)
+        {
+            //after animation go back to chasing state
+
+            EnemyState = State.Chasing;
+            _hasAttackPerformed = false;
+        }
+
 
     }
     void Chase()
     {
-        animator.SetInteger("State", 1);
+        Debug.Log("Chasing player");
+        Agent.isStopped = false;
+        Agent.speed = 3.5f;
 
-        float dist = Vector3.Distance(this.transform.position, _player.transform.position);
-        if (dist > AttackDistance)
+        float dist = Vector3.Distance(this.transform.position, Player.transform.position);
+
+        if (dist > Agent.stoppingDistance)
         {
-            Agent.SetDestination(_player.transform.position);
+            animator.SetInteger("State", 2);
+            Agent.SetDestination(Player.transform.position);
         }
         else
         {
-            EnemyState = State.Attacking;
+
+            if (!_isReadyForAttack)
+            {
+                //Not yet ready for attack
+                animator.SetInteger("State", 0);
+
+
+                _isReadyForAttack = true;
+                _nextTimeAttack = Time.time + 2f;
+
+            }
+            else if (Time.time > _nextTimeAttack)
+            {
+                _isReadyForAttack = false;
+                EnemyState = State.Attacking;
+            }
 
         }
     }
     void Roaming()
     {
-        animator.SetInteger("State", 0);
-        float dist = Agent.remainingDistance;
-        if (dist != Mathf.Infinity && Agent.pathStatus == NavMeshPathStatus.PathComplete && Agent.remainingDistance == 0)
+        Debug.Log("Roaming around");
+        Agent.speed = 1.5f;
+        animator.SetInteger("State", 1);
+
+        if (AgentHasArrived())
         {
+            Debug.Log("Arrived");
             //Agent is at destination
             if (_isWalking)
             {
@@ -108,6 +261,8 @@ public class SwordEnemy : HitAble
             }
             else if (Time.time > _nextTime)
             {
+                Debug.Log("Walk!");
+
                 StartWalking();
             }
         }
@@ -115,9 +270,8 @@ public class SwordEnemy : HitAble
     }
     void StartWalking()
     {
-        _isWalking = true;
 
-        Agent.SetDestination(WalkPlaces[_index]);
+        WalkTo(WalkPlaces[_index]);
         if (_index == WalkPlaces.Count - 1)
         {
 
@@ -129,5 +283,39 @@ public class SwordEnemy : HitAble
         }
 
     }
-    
+    private IEnumerator FOVRoutine()
+    {
+        WaitForSeconds wait = new WaitForSeconds(0.2f);
+
+        while (true)
+        {
+            yield return wait;
+            FieldOfViewCheck();
+        }
+    }
+    private void FieldOfViewCheck()
+    {
+        Collider[] rangeChecks = Physics.OverlapSphere(transform.position, radius, targetMask);
+
+        if (rangeChecks.Length != 0)
+        {
+            Transform target = rangeChecks[0].transform;
+            Vector3 directionToTarget = (target.position - transform.position).normalized;
+
+            if (Vector3.Angle(transform.forward, directionToTarget) < angle / 2)
+            {
+                float distanceToTarget = Vector3.Distance(transform.position, target.position);
+
+                if (!Physics.Raycast(transform.position, directionToTarget, distanceToTarget, obstructionMask))
+                    canSeePlayer = true;
+                else
+                    canSeePlayer = false;
+            }
+            else
+                canSeePlayer = false;
+        }
+        else if (canSeePlayer)
+            canSeePlayer = false;
+    }
+
 }
