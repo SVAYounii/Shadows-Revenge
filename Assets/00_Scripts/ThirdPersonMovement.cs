6239f4ed1e8cdf59bfd61a4ea6a6143221920174
AhmedMuraat <95277929+AhmedMuraat@users.noreply.github.com>
2023-03-16 13:09:09 +0100

    jumping anim

--- Assets/00_Scripts/ThirdPersonMovement.cs
+++ Assets/00_Scripts/ThirdPersonMovement.cs
@@ -21,6 +21,7 @@ public class ThirdPersonMovement : MonoBehaviour
     public float jumpHeight = 2f;
     public float gravity =-4f;
     bool isGrounded;
+    bool isJumping;
     Vector3 velocity;
     void Start()
     {
@@ -34,13 +35,16 @@ public class ThirdPersonMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        isGrounded = Physics.CheckSphere(transform.position, .1f, 1);
-        anim.SetBool("IsGrounded", isGrounded);
+        // Raycast to check if the character is on the ground
+        isGrounded = Physics.Raycast(transform.position, Vector3.down, 0.2f);
+
+        // Set the "IsGrounded" parameter in the Animator
+        anim.SetBool("isGrounded", isGrounded);
+
         if (isGrounded && velocity.y < 0)
         {
             velocity.y = -1;
         }
-
 
         movement = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
         Vector3 direction = new Vector3(movement.x, 0, movement.y).normalized;
@@ -64,7 +68,7 @@ public class ThirdPersonMovement : MonoBehaviour
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
-           
+
             Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
             characterController.Move(moveDirection.normalized * trueSpeed * Time.deltaTime);
             if (sprinting == true)
@@ -76,7 +80,7 @@ public class ThirdPersonMovement : MonoBehaviour
             {
                 anim.SetFloat("Speed", 1);
             }
-          
+
         }
         else
         {
@@ -85,13 +89,28 @@ public class ThirdPersonMovement : MonoBehaviour
         }
         if (Input.GetButtonDown("Jump") && isGrounded)
         {
+            anim.SetBool("isJumping", true);
+            isJumping = true;
             velocity.y = Mathf.Sqrt((jumpHeight * 10) * -2 * gravity);
+        }
+        if (!isGrounded)
+        {
+            isJumping = true;
         }
         if (velocity.y > -20)
         {
             velocity.y += (gravity * 10) * Time.deltaTime;
+
         }
-    
+        else
+        {
+            if (isJumping)
+            {
+                anim.SetBool("isJumping", false);
+                isJumping = false;
+            }
+        }
+
         characterController.Move(velocity * Time.deltaTime);
     }
 }
